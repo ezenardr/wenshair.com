d88654f5eef9e0d4353e73f6b39faac70c8c5bae
ezenardr <ezenardr.dev@gmail.com>
2023-11-13 02:47:15 -0500

    Implemented email sending with nodemailer; added api route for sending email

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from 'next/server';
+const nodemailer = require('nodemailer');
+
+export async function POST(req: NextRequest) {
+    //mail credential
+    const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
+    const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+
+    //request data
+    const { name, email, number, option, dateFormat } = await req.json();
+
+    const transporter = nodemailer.createTransport({
+        host: 'mail.infomaniak.com',
+        port: 465,
+        tls: {
+            ciphers: 'SSLv3',
+            rejectUnauthorized: false,
+        },
+
+        auth: {
+            user: username,
+            pass: password,
+        },
+    });
+
+    try {
+        const mail = await transporter.sendMail({
+            from: username,
+            to: 'ezenardr.dev@gmail.com',
+            subject: `Nouvelle réservation pour Wens-Hair`,
+            html: `
+            <h1 style="text-align : center">Nouvelle Réservation</h1>
+            <p>Nom : ${name}</p>
+            <p>Mail : ${email}</p>
+            <p>Numéro de téléphone : ${number}</p>
+            <p>Option : ${option}</p>
+            <p>Date : ${dateFormat}</p>
+            `,
+        });
+
+        return NextResponse.json({ message: 'Success: email was sent' });
+    } catch (error) {
+        console.log(error);
+        NextResponse.json({ message: 'COULD NOT SEND MESSAGE' });
+    }
+}
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,58 +1,130 @@
 'use client';
 import Style from './form.module.scss';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+
+type DateValue = Date | number | string;
 
 export default function ContactForm() {
-    function submitHandler(e: FormEvent<HTMLFormElement>) {
+    const [name, setName] = useState<string>();
+    const [email, setEmail] = useState<string>();
+    const [number, setNumber] = useState<string>();
+    const [option, setOption] = useState<string>();
+    const [date, setDate] = useState<DateValue>(new Date());
+    async function submitHandler(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log(e);
+        const options: Intl.DateTimeFormatOptions = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+        };
+        const newDate = new Date(date);
+        const dateFormat = newDate.toLocaleDateString('fr-FR', options);
+        try {
+            const res = await fetch('http://localhost:3000/api/contact', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name,
+                    email,
+                    number,
+                    option,
+                    dateFormat,
+                }),
+            });
+            if (!res.ok) console.log('failed');
+            console.log('success');
+        } catch (error) {
+            console.log(error);
+        }
     }
     return (
         <form className={Style.form} onSubmit={(e) => submitHandler(e)}>
             <div className={Style.inputBox}>
-                <input type="text" name="Nom" required />
+                <input
+                    type="text"
+                    name="Nom"
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
                 <label htmlFor="Nom">Nom et prénom</label>
             </div>
             <div className={Style.inputBox}>
-                <input type="email" name="email" required />
+                <input
+                    type="email"
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
                 <label htmlFor="email">Email</label>
             </div>
             <div className={Style.inputBox}>
-                <input type="tel" name="phone" required />
+                <input
+                    type="tel"
+                    name="phone"
+                    onChange={(e) => setNumber(e.target.value)}
+                    required
+                />
                 <label htmlFor="phone">Téléphone</label>
             </div>
 
             <div className={Style.wrapper}>
                 <div className={Style.card}>
-                    <input type="radio" name="options" value="simple" />
+                    <input
+                        type="radio"
+                        name="options"
+                        value="simple"
+                        onChange={(e) => setOption(e.target.value)}
+                    />
                     <span className={Style.check}></span>
                     <label htmlFor="options" className={Style.label}>
                         BASIC
                     </label>
                 </div>
                 <div className={Style.card}>
-                    <input type="radio" name="options" value="barbe" />
+                    <input
+                        type="radio"
+                        name="options"
+                        value="barbe"
+                        onChange={(e) => setOption(e.target.value)}
+                    />
                     <span className={Style.check}></span>
                     <label htmlFor="options" className={Style.label}>
                         +BARBE
                     </label>
                 </div>
                 <div className={Style.card}>
-                    <input type="radio" name="options" value="sourcils" />
+                    <input
+                        type="radio"
+                        name="options"
+                        value="sourcils"
+                        onChange={(e) => setOption(e.target.value)}
+                    />
                     <span className={Style.check}></span>
                     <label htmlFor="options" className={Style.label}>
                         SOURCILS
                     </label>
                 </div>
                 <div className={Style.card}>
-                    <input type="radio" name="options" value="contour" />
+                    <input
+                        type="radio"
+                        name="options"
+                        value="contour"
+                        onChange={(e) => setOption(e.target.value)}
+                    />
                     <span className={Style.check}></span>
                     <label htmlFor="options" className={Style.label}>
                         CONTOUR
                     </label>
                 </div>
                 <div className={Style.card}>
-                    <input type="radio" name="options" value="contour" />
+                    <input
+                        type="radio"
+                        name="options"
+                        value="autres"
+                        onChange={(e) => setOption(e.target.value)}
+                    />
                     <span className={Style.check}></span>
                     <label htmlFor="options" className={Style.label}>
                         Autres+
@@ -63,6 +135,8 @@ export default function ContactForm() {
                 type="datetime-local"
                 aria-label="date"
                 className={Style.date}
+                onChange={(e) => setDate(e.target.value)}
+                required
             />
             <button type="submit">
                 ENVOYER
